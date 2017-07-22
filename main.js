d47e1a20b01684856e641786e8c956c0b4d8b9c6
mayudreamzz <wijerathnasanju@gmail.com>
2017-07-22 14:17:30 +0530

    User Panel

---
+++ main.js
@@ -0,0 +1,46 @@
+function homebutton() {
+	document.getElementById("msgtxt").innerHTML= "NSBM Green University Town";
+	bodymsgdiv1.style.background="black";
+	document.getElementById("msgtxt1").innerHTML= "Experience the Future of Higher Education";
+
+}
+function mouseleave() {
+	document.getElementById("msgtxt").innerHTML= "";
+	document.getElementById("msgtxt1").innerHTML= "";
+	bodymsgdiv1.style.background="none";
+}
+function aboutbutton() {
+	document.getElementById("msgtxt").innerHTML= "Imagine Your Future With No Limits!";
+	bodymsgdiv1.style.background="black";
+	document.getElementById("msgtxt1").innerHTML= "From exemplary teaching to a campus experience packed with cultural and social opportunities, NSBM is the best place to learn and get your future off to a flying start...";
+}
+function servicebutton() {
+	document.getElementById("msgtxt").innerHTML= "Featured Services";
+	bodymsgdiv1.style.background="black";
+	document.getElementById("msgtxt1").innerHTML= "Career Guidance and Industrial Training"
+}
+function stubutton() {
+	document.getElementById("msgtxt").innerHTML= "Your Area";
+	bodymsgdiv1.style.background="black";
+	document.getElementById("msgtxt1").innerHTML= "All the Important things for the Students";
+}
+function supportbutton() {
+	document.getElementById("msgtxt").innerHTML= "Contact Us";
+	document.getElementById("msgtxt1").innerHTML= "We are looking forward to hearing from you soon...";
+	bodymsgdiv1.style.background="black";
+}
+function loginbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Log In";
+}
+function regbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Register";
+}
+function mouseleav() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+}
+function backbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Previous Page";
+}
+function nextbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Next Page";
+}

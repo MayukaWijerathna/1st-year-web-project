72e12cdb57f07d478fd8938392db524ecf693212
anuhasdissanayake <30366770+anuhasdissanayake@users.noreply.github.com>
2017-07-22 15:46:57 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,40 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Module Descriptor";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "SE16.2_Lecture 01";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Lecture 02- OOP - Classes and Objects";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+function mouseleaveb5() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b5.style.background="#008CBA";
+}
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Lec03_OOP_Encapsulation";
+	categorydiv1b4.style.background="#4CAF50";
+}
+function sogbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Lec04_OOP_Inheritance_Abstraction";
+	categorydiv1b5.style.background="#4CAF50";
+}

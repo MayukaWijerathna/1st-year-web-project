14416b3ece267f06ec39c1f8134ab43100d8b1f9
Devni Piyathma <vgdpiyathma95@gmail.com>
2017-07-22 15:45:07 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,24 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Important News";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Additional Events";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Assignment Submit";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}

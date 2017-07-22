f9d1c8dfd314667c04b66f32a0cc8b6441bf1d24
KalinduMR <kmudithya@gmail.com>
2017-07-22 15:57:50 +0530

    Add files via upload

---
+++ service.php
@@ -0,0 +1,176 @@
+<?php 
+	session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS - Learning Management System</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<script language="javascript" type="text/javascript" src="main.js"></script>
+	<link rel="stylesheet" type="text/css" href="service.css">
+	<script language="javascript" type="text/javascript" src="service.js"></script>
+	<link rel="icon" type="image/png" href="pictures/favicon-16x16.png" sizes="16x16">
+
+</head>
+<body>
+	<div class="background-wrap">
+		<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+		<source src="login.mp4" type="video/mp4" >
+		</video>
+	</div>
+		<div id="maindiv">
+		
+		<div id="navbardiv">
+		 	
+		 	<div id="navspacediv1">
+		 		
+		 	</div>
+
+		 	<div id="logodiv1">
+		 		<center>
+		 		<img id="logo1" src="pictures/logo.png">
+		  		</center>
+		 	</div>
+		 	
+		 	<div id="navtispace"></div>
+		 	<div id="navtispace1"></div>
+		 	
+		 	<div id="navtitlediv">
+		 		<center><br>Learning Management System</center>
+		 	</div>
+
+		 	<a href="home.php" id="navbuttontxt"><div id="navbutton" onmouseover="homebutton()"; onmouseout="mouseleave();">
+		 		HOME
+		 	</div></a><br>
+		 	<a  href="about.php" id="navbuttontxt"><div id="navbutton" onmouseover="aboutbutton()"; onmouseout="mouseleave();">
+		 		ABOUT US
+		 	</div></a><br>
+		 	<a href="service.php" id="navbuttontxt"><div id="navbutton" onmouseover="servicebutton()"; onmouseout="mouseleave();">
+		 		SERVICES
+		 	</div></a><br>
+		 	<a href="student.php" id="navbuttontxt"><div id="navbutton" onmouseover="stubutton()"; onmouseout="mouseleave();">
+		 		STUDENT AREA
+		 	</div></a><br>
+		 	<a href="support.php" id="navbuttontxt"><div id="navbutton" onmouseover="supportbutton()"; onmouseout="mouseleave();">
+		 		SUPPORT
+		 	</div></a><br><br><hr><br>
+		 	<div id="navbutton1">
+		 		<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<a href="logout.php" id="navbuttontxt1">LOGOUT</a>';
+
+						          }else {
+						            echo '<a href="index.php" id="navbuttontxt1">LOGIN</a>';
+						          }
+
+						         ?>	
+		 	</div><br>
+		 	<div id="navbutton2">
+		 		<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<a href="account.php" id="navbuttontxt">ACCOUNT</a>';
+
+						          }else {
+						            echo '<a href="register.php" id="navbuttontxt">REGISTER</a>';
+						          }
+
+						         ?>	
+		 	</div>
+		 	
+
+		</div>
+		
+		<div id="bodydiv">
+				
+				<div id="bodydiv1">
+					<div id="bodydiv11">
+
+					</div>
+					<div id="bodydiv12">
+						<div id="bodydiv111"></div>
+						<div id="bodydiv112"></div>
+						<div id="bodymsgdiv1">
+							<a id="msgtxt"></a><br><br><a id="msgtxt1"></a>
+						</div>
+					</div>
+					<div id="bodydiv13">
+						<div id="bodydiv13a"></div>
+						<div id="bodydiv13b"></div>	
+						<div id="bodydiv13c">
+
+							<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<img id="userimg" src="pictures/user.png">';
+						          }else {
+						            echo '<img id="userimg" src="pictures/guest.png">';
+						          }
+
+						         ?>
+							
+						</div>
+						<div id="bodydiv13b"></div>
+						<div id="bodydiv13d"></div>
+						<div id="bodydiv13e"></div>
+						<div id="bodydiv13f">
+							
+								
+								<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<p id="username">'.$_SESSION['username'].'</p>';
+						          }else {
+						            echo '<p id="username"> GUEST </p>';
+						          }
+
+						         ?>
+
+							
+						</div>
+
+					</div>
+				</div>
+
+				<div id="bodylinediv1"></div>
+
+				<div id="bodydiv2">
+					<div id="bodydiv21"></div><div id="bodydiv23"></div>
+					<div id="bodydiva">
+						<div id="bmargindiv"></div>
+						<div id="bmargindiv3"></div>
+						<div id="bmargindiv2">
+							<a id="btitle1">SERVICES</a><br><br>
+							<div id="bline1"></div>
+							<br><br>
+							<p id="bcontent1"><b><u>Introduction To Industrial Training</u></b><br><br>
+							In today's context, whilst a Degree is not a guarantee for a succesful employment opportunity, it is a reliable indicator of an individual's competency. On the other hand graduates must update their knowledge, practical experience, soft skills and attitudes to be competitive in the job market. Thus the industrial training is a vital component which improves competency of a graduate as a corporate leader.
+							</p>
+
+							<p id="bcontent1">
+							Industrial Training Programme (ITP) is designed to provide the students with a supervised practical training withina specified time-frame. The training can be carried out in a government or private sector organization, and it includes the job training as well as the research based training.</p>
+
+							<p id="bcontent1"><b><u>
+							Aims of the Programme</u></b><br><br>
+							The main aim of the ITP is to produce graduates who are ready to face the industry. The programme also expects to develop competent graduates who are able to apply the knowledge acquired at the institute in a practical working environment. By exploring the real working experience, students under- stand expectations and guidelines of the business arena which lead to improve the chance of employment after graduation. This is a continuous effort done by National School of Business Management to eliminate the gap between academia and industry. Feedback received from employers who are part of the programme is used to improve the existing curriculum in line with the needs of the industry.</p>
+
+
+						</div>
+					</div>
+				</div>
+
+				<div id="bodydiv3">
+					<div id="bodydiv31"></div>
+					<div id="bodydiv32">
+						<div id="bodydiv321"></div>
+						<div id=navtxtmsgdiv><a id="navtxtmsg">NSBM Green University Town</a></div>
+					</div>
+					<div id="bodydiv331" onmouseover="backbutton()"; onmouseout="mouseleav();">
+						<a id="textnavb"><</a>
+					</div>
+					<div id="bodydiv34"></div>
+					<div id="bodydiv332" onmouseover="nextbutton()"; onmouseout="mouseleav();">
+						<a href="service2.php" id="textnavb">></a>
+					</div>
+				</div>
+		</div>
+
+</body>
+</html>
---
+++ service3.php
@@ -0,0 +1,173 @@
+<?php 
+	session_start();
+ ?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>LMS - Learning Management System</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<script language="javascript" type="text/javascript" src="main.js"></script>
+	<link rel="stylesheet" type="text/css" href="service3.css">
+	<script language="javascript" type="text/javascript" src="service.js"></script>
+	<link rel="icon" type="image/png" href="pictures/favicon-16x16.png" sizes="16x16">
+
+</head>
+<body>
+	<div class="background-wrap">
+		<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+		<source src="login.mp4" type="video/mp4" >
+		</video>
+	</div>
+		<div id="maindiv">
+		
+		<div id="navbardiv">
+		 	
+		 	<div id="navspacediv1">
+		 		
+		 	</div>
+
+		 	<div id="logodiv1">
+		 		<center>
+		 		<img id="logo1" src="pictures/logo.png">
+		  		</center>
+		 	</div>
+		 	
+		 	<div id="navtispace"></div>
+		 	<div id="navtispace1"></div>
+		 	
+		 	<div id="navtitlediv">
+		 		<center><br>Learning Management System</center>
+		 	</div>
+
+		 	<a href="home.php" id="navbuttontxt"><div id="navbutton" onmouseover="homebutton()"; onmouseout="mouseleave();">
+		 		HOME
+		 	</div></a><br>
+		 	<a  href="about.php" id="navbuttontxt"><div id="navbutton" onmouseover="aboutbutton()"; onmouseout="mouseleave();">
+		 		ABOUT US
+		 	</div></a><br>
+		 	<a href="service.php" id="navbuttontxt"><div id="navbutton" onmouseover="servicebutton()"; onmouseout="mouseleave();">
+		 		SERVICES
+		 	</div></a><br>
+		 	<a href="student.php" id="navbuttontxt"><div id="navbutton" onmouseover="stubutton()"; onmouseout="mouseleave();">
+		 		STUDENT AREA
+		 	</div></a><br>
+		 	<a href="support.php" id="navbuttontxt"><div id="navbutton" onmouseover="supportbutton()"; onmouseout="mouseleave();">
+		 		SUPPORT
+		 	</div></a><br><br><hr><br>
+		 	<div id="navbutton1">
+		 		<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<a href="logout.php" id="navbuttontxt1">LOGOUT</a>';
+
+						          }else {
+						            echo '<a href="index.php" id="navbuttontxt1">LOGIN</a>';
+						          }
+
+						         ?>	
+		 	</div><br>
+		 	<div id="navbutton2">
+		 		<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<a href="account.php" id="navbuttontxt">ACCOUNT</a>';
+
+						          }else {
+						            echo '<a href="register.php" id="navbuttontxt">REGISTER</a>';
+						          }
+
+						         ?>	
+		 	</div>
+		 	
+
+		</div>
+		
+		<div id="bodydiv">
+				
+				<div id="bodydiv1">
+					<div id="bodydiv11">
+
+					</div>
+					<div id="bodydiv12">
+						<div id="bodydiv111"></div>
+						<div id="bodydiv112"></div>
+						<div id="bodymsgdiv1">
+							<a id="msgtxt"></a><br><br><a id="msgtxt1"></a>
+						</div>
+					</div>
+					<div id="bodydiv13">
+						<div id="bodydiv13a"></div>
+						<div id="bodydiv13b"></div>	
+						<div id="bodydiv13c">
+
+							<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<img id="userimg" src="pictures/user.png">';
+						          }else {
+						            echo '<img id="userimg" src="pictures/guest.png">';
+						          }
+
+						         ?>
+							
+						</div>
+						<div id="bodydiv13b"></div>
+						<div id="bodydiv13d"></div>
+						<div id="bodydiv13e"></div>
+						<div id="bodydiv13f">
+							
+								
+								<?php 
+						          if (isset($_SESSION['username'])){
+						            echo '<p id="username">'.$_SESSION['username'].'</p>';
+						          }else {
+						            echo '<p id="username"> GUEST </p>';
+						          }
+
+						         ?>
+
+							
+						</div>
+
+					</div>
+				</div>
+
+				<div id="bodylinediv1"></div>
+
+				<div id="bodydiv2">
+					<div id="bodydiv21"></div><div id="bodydiv23"></div>
+					<div id="bodydiva">
+						<div id="bmargindiv"></div>
+						<div id="bmargindiv3"></div>
+						<div id="bmargindiv2">
+							<a id="btitle1">SERVICES</a><br><br>
+							<div id="bline1"></div>
+							<br><br>
+							<p id="bcontent1"><b><u>Scope Of The Industrial Training</u></b><br><br>
+							Job duties of trainees can be decided by the employer based on their competencies in relation to specific fields. During the period of training, students must be supervised by respective employer in order to ensure that they receive relevant guidance. NSBM expects constructive feedback of their progress during the period of industrial training. It is compulsory to complete industrial training in order to complete the Degree programmes.
+							</p>
+
+							<p id="bcontent1"><B><U>Duration of the Industrial Training</U></B><br><br>
+							Six (6) months are assigned for industrial training and employer is free to extend the period of employment after completion of industrial training.</p>
+
+							
+
+						</div>
+					</div>
+				</div>
+
+				<div id="bodydiv3">
+					<div id="bodydiv31"></div>
+					<div id="bodydiv32">
+						<div id="bodydiv321"></div>
+						<div id=navtxtmsgdiv><a id="navtxtmsg">NSBM Green University Town</a></div>
+					</div>
+					<div id="bodydiv331" onmouseover="backbutton()"; onmouseout="mouseleav();">
+						<a href="service2.php" id="textnavb"><</a>
+					</div>
+					<div id="bodydiv34"></div>
+					<div id="bodydiv332" onmouseover="nextbutton()"; onmouseout="mouseleav();">
+						<a id="textnavb">></a>
+					</div>
+				</div>
+		</div>
+
+</body>
+</html>

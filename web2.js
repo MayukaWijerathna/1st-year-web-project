18dce4dc893a608556afd41b35d8e2e7c7c27c84
anuhasdissanayake <30366770+anuhasdissanayake@users.noreply.github.com>
2017-07-22 15:36:59 +0530

    Add files via upload

---
+++ web2.js
@@ -0,0 +1,40 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Lecturer 2 Cascading Style Sheets (CSS)";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "CSS Properties";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Page Sections and the CSS Box Model";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+function mouseleaveb5() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b5.style.background="#008CBA";
+}
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Floating and Positioning";
+	categorydiv1b4.style.background="#4CAF50";
+}
+function sogbutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Lecturer 3 Javascript";
+	categorydiv1b5.style.background="#4CAF50";
+}

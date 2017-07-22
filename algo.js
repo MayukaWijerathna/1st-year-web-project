34fbf0f93ab254145d936e0898304545992faa29
KalinduMR <kmudithya@gmail.com>
2017-07-22 15:33:52 +0530

    Add files via upload

---
+++ algo.js
@@ -0,0 +1,40 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Module Descriptor";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "  Introduction ";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Module Introduction";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+function mouseleaveb5() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b5.style.background="#008CBA";
+}
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Analysis of Algorithms";
+	categorydiv1b4.style.background="#4CAF50";
+}
+function sogbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Searching Algorithms";
+	categorydiv1b5.style.background="#4CAF50";
+}
---
+++ c2.js
@@ -0,0 +1,40 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Conditions";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Switch case and goto ";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Tute - If Conditions";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+function mouseleaveb5() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b5.style.background="#008CBA";
+}
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "Loops";
+	categorydiv1b4.style.background="#4CAF50";
+}
+function sogbutton() {
+	document.getElementById("navtxtmsg").innerHTML= " Tute - Loops";
+	categorydiv1b5.style.background="#4CAF50";
+}

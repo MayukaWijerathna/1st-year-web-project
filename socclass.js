33e9d1a1c5a582dcb44f3118c7e6f56bcbaeb52c
Mayuka Wijerathna <wijerathnasanju@gmail.com>
2017-07-22 15:55:28 +0530

    Add files via upload

---
+++ socclass.js
@@ -0,0 +1,40 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "16.1 Group-A";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "16.1 Group-B";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= "16.2 Group-A1";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+function mouseleaveb5() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b5.style.background="#008CBA";
+}
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "16.2 Group-A2";
+	categorydiv1b4.style.background="#4CAF50";
+}
+function sogbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "16.2 Group-B1";
+	categorydiv1b5.style.background="#4CAF50";
+}
---
+++ soccourse.js
@@ -0,0 +1,33 @@
+function socbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "1st Year";
+	categorydiv1b1.style.background="#4CAF50";
+}
+function mouseleaveb1() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b1.style.background="#008CBA";
+}
+function sobbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "2nd Year";
+	categorydiv1b2.style.background="#4CAF50";
+}
+function soebutton() {
+	document.getElementById("navtxtmsg").innerHTML= "3rd Year";
+	categorydiv1b3.style.background="#4CAF50";
+}
+function mouseleaveb2() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b2.style.background="#008CBA";
+}
+function mouseleaveb3() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b3.style.background="#008CBA";
+}
+function mouseleaveb4() {
+	document.getElementById("navtxtmsg").innerHTML= "NSBM Green University Town";
+	categorydiv1b4.style.background="#008CBA";
+}
+
+function sofbutton() {
+	document.getElementById("navtxtmsg").innerHTML= "4th Year";
+	categorydiv1b4.style.background="#4CAF50";
+}
